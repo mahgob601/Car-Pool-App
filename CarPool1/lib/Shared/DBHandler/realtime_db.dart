6ef5e211591b970f0eb10e3d0eec0dc9eb57638f
Yassin Mahgoub <mahgob601@gmail.com>
2023-12-22 22:29:24 +0200

    update

--- CarPool1/lib/Shared/DBHandler/realtime_db.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:car_pool1/firebase_options.dart';
-import 'package:firebase_database/firebase_database.dart';
-
-class RealTimeDB{
-
-}
